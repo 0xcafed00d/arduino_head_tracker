f723738a5f07a14d194f7b086f3ae77c6d78447f
Lee Witek <neomech@gmail.com>
2018-06-16 13:48:53 +0100

    adding radio module to motion sensor

--- motion_sensor/src/bno055_impl.cpp
+++ motion_sensor/src/bno055_impl.cpp
@@ -5,15 +5,14 @@
 
 s8 I2C_bus_read(u8 dev_addr, u8 reg_addr, u8* reg_data, u8 cnt) {
 	s8 written = 0;
-	Wire.beginTransmission(dev_addr);  // Start of transmission
-	Wire.write(reg_addr);              // Desired start register
-	written = Wire.endTransmission();  // Stop of transmission
-	delayMicroseconds(150);            // Caution Delay
-	Wire.requestFrom(dev_addr, cnt);   // Request data
-	while (Wire.available())           // The slave device may send less than requested (burst read)
-	{
-		*reg_data = Wire.read();  // Receive a byte
-		reg_data++;               // Increment pointer
+	Wire.beginTransmission(dev_addr);
+	Wire.write(reg_addr);
+	written = Wire.endTransmission();
+	delayMicroseconds(150);
+	Wire.requestFrom(dev_addr, cnt);
+	while (Wire.available()) {
+		*reg_data = Wire.read();
+		reg_data++;
 	}
 	return written;
 }
@@ -24,15 +23,13 @@ void delay_msec(u32 ms) {
 
 s8 I2C_bus_write(u8 dev_addr, u8 reg_addr, u8* reg_data, u8 cnt) {
 	s8 comres = 0;
-	Wire.beginTransmission(dev_addr);  // Start of transmission
-	Wire.write(reg_addr);              // Desired start register
-	for (unsigned char index = 0; index < cnt;
-	     index++)  // Note that the BNO055 supports burst write
-	{
-		Wire.write(*reg_data);  // Write the data
-		reg_data++;             // Increment pointer
+	Wire.beginTransmission(dev_addr);
+	Wire.write(reg_addr);
+	for (unsigned char index = 0; index < cnt; index++) {
+		Wire.write(*reg_data);
+		reg_data++;
 	}
-	comres = Wire.endTransmission();  // Stop of transmission
-	delayMicroseconds(150);           // Caution Delay
+	comres = Wire.endTransmission();
+	delayMicroseconds(150);
 	return comres;
 }
--- motion_sensor/src/main.cpp
+++ motion_sensor/src/main.cpp
@@ -2,45 +2,56 @@
 #include <RF24.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <printf.h>
 
 #include "bno055_impl.h"
 #include "log.h"
 
+Logger loggr(Serial);
+
+// ---------------------------------------------------------------
+// RF24 Config
+// ---------------------------------------------------------------
 /*
-        rf24	Arduino leonardo
+        rf24	Arduino uno
 
     1	GND		GND
     2	3.3v	3.3v
-    3	CE		SDA (Ardiuno pin 2)
-    4	CSN		SCL (Ardiuno pin 3)
-    5	SCK		SCK
-    6	MOSI	MOSI
-    7	MISO	MISO
+    3	CE		pin 10
+    4	CSN		pin 9
+    5	SCK		SCK  pin 13
+    6	MOSI	MOSI pin 11
+    7	MISO	MISO pin 12
     8	NC
 */
+static const int PIN_RF24_CE = 10;
+static const int PIN_RF24_CSN = 9;
 
-RF24 radio(2, 3);
+RF24 radio(PIN_RF24_CE, PIN_RF24_CSN);
 
 byte addresses[][6] = {"1Node", "2Node"};
 
+void setup_rf24() {
+	radio.begin();
+	radio.printDetails();
+}
+
+// ---------------------------------------------------------------
+// BNO055 Config
+// ---------------------------------------------------------------
 bno055_t bno055 = {0};
+float zeroHeading = 0;
 
-static const int BNO055_RESET_PIN = A3;
-static const int ZERO_HEADING_PIN = 4;
+static const int PIN_BNO055_RESET = A3;
+static const int PIN_ZERO_HEADING = 4;
 
-Logger loggr(Serial);
+void setup_bno055() {
+	pinMode(PIN_ZERO_HEADING, INPUT_PULLUP);
+	pinMode(PIN_BNO055_RESET, OUTPUT);
 
-void setup() {
-	Serial.begin(115200);
-
-	Wire.begin();
-
-	pinMode(ZERO_HEADING_PIN, INPUT_PULLUP);
-	pinMode(BNO055_RESET_PIN, OUTPUT);
-
-	digitalWrite(BNO055_RESET_PIN, LOW);
+	digitalWrite(PIN_BNO055_RESET, LOW);
 	delay_msec(300);
-	digitalWrite(BNO055_RESET_PIN, HIGH);
+	digitalWrite(PIN_BNO055_RESET, HIGH);
 	delay_msec(700);
 
 	bno055.delay_msec = delay_msec;
@@ -49,13 +60,20 @@ void setup() {
 	bno055.dev_addr = 0x28;
 
 	auto res = bno055_init(&bno055);
-
 	loggr << res << bno055.accel_rev_id << bno055.sw_rev_id;
 
 	bno055_set_operation_mode(BNO055_OPERATION_MODE_NDOF_FMC_OFF);
 }
 
-float zeroHeading = 0;
+void setup() {
+	Serial.begin(115200);
+	Wire.begin();
+	SPI.begin();
+	printf_begin();
+
+	setup_bno055();
+	setup_rf24();
+}
 
 void loop() {
 	u8 sys_cal_stat = 0;
@@ -74,7 +92,7 @@ void loop() {
 
 	delay(50);
 
-	if (digitalRead(ZERO_HEADING_PIN) == 0) {
+	if (digitalRead(PIN_ZERO_HEADING) == 0) {
 		zeroHeading = f.h;
 	}
 
